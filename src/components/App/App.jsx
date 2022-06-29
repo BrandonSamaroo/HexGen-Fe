d0cf1b65b0ccf0c0243814a9b8b77a25dc04df38
Brandon Samaroo <samaroo.bran@gmail.com>
2022-06-29 11:21:26 -0400

    added header base

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import '../ColorPicker/ColorPicker'
 import ColorPicker from '../ColorPicker/ColorPicker'
+import Header from '../Header/Header'
 
 export default function App() {
 
   return (
     <div className="App">
+      <Header/>
       <ColorPicker/>
     </div>
   )
